294c2dc3d83b499d6c557853516a7ed810500e81
miyasho <miyashi405@gmail.com>
2017-04-03 18:58:09 +0900

    昔の表記になっていた部分（audio）を修正した

--- juliusTestUnity/Assets/julius/Script/microphone.cs
+++ juliusTestUnity/Assets/julius/Script/microphone.cs
@@ -8,13 +8,13 @@ public class microphone : MonoBehaviour {
 	public float[] spectrum;
 	public float max = 1;
 	public float min = 1;
-
+    AudioSource _audio;
 
 	private float GetAveragedVolume()
 	{ 
 		float[] data = new float[256];
 		float a = 0;
-		audio.GetOutputData(data,0);
+		_audio.GetOutputData(data,0);
 		
 		foreach(float s in data)
 		{
@@ -26,18 +26,18 @@ public class microphone : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		audio.clip = Microphone.Start(null, true, 999, 44100);  // マイクからのAudio-InをAudioSourceに流す
-		audio.loop = true;                                      // ループ再生にしておく
-		audio.mute = true;                                      // マイクからの入力音なので音を流す必要がない
+		_audio.clip = Microphone.Start(null, true, 999, 44100);  // マイクからのAudio-InをAudioSourceに流す
+		_audio.loop = true;                                      // ループ再生にしておく
+		_audio.mute = true;                                      // マイクからの入力音なので音を流す必要がない
 		while (!(Microphone.GetPosition("") > 0)){}             // マイクが取れるまで待つ。空文字でデフォルトのマイクを探してくれる
-		audio.Play(); 
+		_audio.Play(); 
 
 		spectrum = new float[1024];
 	}
 
 	// Update is called once per frame
 	void Update () {
-		spectrum = audio.GetSpectrumData(1024, 0, FFTWindow.BlackmanHarris);
+		spectrum = _audio.GetSpectrumData(1024, 0, FFTWindow.BlackmanHarris);
 		vol = GetAveragedVolume();
 
 		if(vol > max){
